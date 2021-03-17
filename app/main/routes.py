fccb79166b82574a2c67fafeb2b6ba0ee5b8a43f
johnnyquinn <jftq97@gmail.com>
2021-03-16 20:35:35 -0400

    [add] search() and champ() routes, updated base and homepage htmls, and created navbar and search input templates

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,3 +7,16 @@ main = Blueprint("app", __name__)
 def homepage():
     
     return render_template('homepage.html')
+
+@main.route('/search', methods=['GET'])
+def search():
+    champ_query = request.args.get('champ_query')
+
+    print(f'-------------------------------------------------------------------search: {champ_query}')
+
+    return redirect(url_for('app.champ', champ=champ_query))
+
+@main.route('/champ/<champ>')
+def champ(champ):
+    
+    return render_template('champ.html')
